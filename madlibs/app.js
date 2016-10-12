149f6ddf79fedd8b9796b067fcb659fcf91a1093
Samantha Spensley <spenslsm@gmail.com>
2016-10-12 16:57:21 -0600

    fixed controller issues with display/hide

--- madlibs/app.js
+++ madlibs/app.js
@@ -1,23 +1,13 @@
 var app = angular.module('app', []);
 
 app.controller('MainController', function($scope) {
-  $scope.boy = '';
-  $scope.adjective = '';
-  $scope.noun1 = '';
-  $scope.noun2 = '';
-  $scope.noun3 = '';
-  $scope.insect = '';
-  $scope.verb = '';
-  $scope.submit = function() {
-    $scope.boy = boy;
-    $scope.adjective = adjective;
-    $scope.noun1 = noun1;
-    $scope.noun2 = noun2;
-    $scope.noun3 = noun3;
-    $scope.insect = insect;
-    $scope.verb = verb;
-  };
-  $scope.reset = function() {
+
+  $scope.ShowHide = function() {
+    $scope.IsVisible = $scope.IsVisible ? false: true;
+  }
+
+  $scope.ShowHideAndEmpty = function() {
+    $scope.IsVisible = $scope.IsVisible ? false: true;
     $scope.boy = '';
     $scope.adjective = '';
     $scope.noun1 = '';
